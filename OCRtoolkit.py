9644c96727dd3659e63c8f7d7bb42e7ac24be434
Jedirainsberger <andrewrainsberger@gmail.com>
2023-07-08 18:39:34 -0600

    Update OCRtoolkit.py

--- OCRtoolkit.py
+++ OCRtoolkit.py
@@ -1,3 +1,4 @@
+import logging
 from abc import ABC
 from superagi.tools.base_tool import BaseToolkit, BaseTool
 from typing import Type, List
@@ -11,9 +12,15 @@ from ocr_tool import OcrTool
 class OCRToolkit(BaseToolkit, ABC):
     name: str = "OCR Toolkit"
     description: str = "OCR Tool kit contains all tools related to OCR with GPT-4"
+    
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
 
     def get_tools(self) -> List[BaseTool]:
+        self.logger.debug('Getting tools')
         return [OcrTool()]
 
     def get_env_keys(self) -> List[str]:
+        self.logger.debug('Getting environment keys')
         return ["OPENAI_API_KEY"]
